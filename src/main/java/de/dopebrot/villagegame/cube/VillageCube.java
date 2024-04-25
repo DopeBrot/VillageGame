8facd1547baa9f9bf96b9f299868db4ff9b90a98
Hptlo <niclaselling8@gmail.com>
2024-04-25 22:29:04 +0200

    added VillageCube.java

---
+++ src/main/java/de/dopebrot/villagegame/cube/VillageCube.java
@@ -0,0 +1,12 @@
+package de.dopebrot.villagegame.cube;
+
+import de.dopebrot.villagegame.structure.Structure;
+
+public abstract class VillageCube {
+
+	Structure structure;
+
+	public VillageCube() {
+	}
+
+}
